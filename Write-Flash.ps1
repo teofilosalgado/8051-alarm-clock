05ad5fee21eca16ccea7f2600780c423e50c2935
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2021-12-18 15:32:18 -0300

    Fixing upload script.

--- Write-Flash.ps1
+++ Write-Flash.ps1
@@ -1,5 +1,6 @@
 $ConfigurationFilePath = Join-Path -Path $PSScriptRoot -ChildPath "avrdude.conf"
 $BinaryFilePath = Join-Path -Path $PSScriptRoot -ChildPath "Objects/8081-alarm-clock.hex"
-$FlashCommand = "avrdude -v -C $ConfigurationFilePath -c stk500v1 -p 89s51 -P COM3 -b 19200 -U flash:w:$BinaryFilePath:i"
+$FlashCommand = "avrdude -v -C ""$ConfigurationFilePath"" -c stk500v1 -p 89s51 -P COM3 -b 19200 -U flash:w:""$BinaryFilePath"":i"
 
+Write-Output "Executing: $FlashCommand"
 Invoke-Expression $FlashCommand
