72424b893a19011b6534cfdb4196bb621031d536
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2021-12-18 17:31:34 -0300

    Using Arduindo IDE based avrdude configuration file.

--- Write-Flash.ps1
+++ Write-Flash.ps1
@@ -1,5 +1,5 @@
 $ConfigurationFilePath = Join-Path -Path $PSScriptRoot -ChildPath "avrdude.conf"
-$BinaryFilePath = Join-Path -Path $PSScriptRoot -ChildPath "Objects/8081-alarm-clock.hex"
+$BinaryFilePath = Join-Path -Path $PSScriptRoot -ChildPath "Objects/8051-alarm-clock.hex"
 $FlashCommand = "avrdude -v -C ""$ConfigurationFilePath"" -c stk500v1 -p 89s51 -P COM3 -b 19200 -U flash:w:""$BinaryFilePath"":i"
 
 Write-Output "Executing: $FlashCommand"
